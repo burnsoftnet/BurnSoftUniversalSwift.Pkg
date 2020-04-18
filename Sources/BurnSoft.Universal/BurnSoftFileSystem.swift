0f1c99304199db3e58b8d48d40c7828da7095577
Joe M <joe.mireles@burnsoft.net>
2020-04-18 14:34:51 -0400

    #3 added function to create directory

--- Sources/BurnSoft.Universal/BurnSoftFileSystem.swift
+++ Sources/BurnSoft.Universal/BurnSoftFileSystem.swift
@@ -13,6 +13,18 @@ import Foundation
 ///  General class that have misc file operations that have been used in our applications such as, copy file, delete file create directory, get file extension, etc
 open class BurnSoftFileSystem: NSObject
 {
+    
+    /**
+     Simplify the copy and replace method with overwriteoption
+     
+     - Parameter
+            - fromPath :the source path and file that you want to copy
+            - toPath - new location to copy to
+     
+     - Throws: returns errot in msg string
+     
+     - Returns: true if it was able to copy the file, false if it was unable to.
+     */
     open class func copyFileFromFilePath(fromPath: String, toPath: String, msg :inout String) -> Bool
     {
         let bAns : Bool = false
@@ -27,14 +39,29 @@ open class BurnSoftFileSystem: NSObject
         }
         return bAns
     }
+    
+    /**
+     Create a directory if it doesn't already exist
+     
+     - Parameter path : full path of the new directory that you want to create
+     
+     - Throws:Error message in msg
+     
+     - Returns:true if it was able to create the directory, false if it was unable to
+     */
     open class func createDirectoryIfNotExists(path : String, msg :inout String) -> Bool
     {
-        let bAns : Bool = false
-        let error : NSError
-        let isDir : Bool = false
+        var bAns : Bool = false
+        var isDir : ObjCBool = false
         let fileManager : FileManager = FileManager()
-        if  !FileManager.fileExists(path)
+        if  !fileManager.fileExists(atPath: path, isDirectory: &isDir)
         {
+            do {
+                try fileManager.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
+                bAns = true
+            } catch  {
+                msg = "Unexpected error: \(error)."
+            }
             
         }
         return bAns
