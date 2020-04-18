c48aa562f2022bb0ff5c90b914ee62cce0f2a071
Joe M <joe.mireles@burnsoft.net>
2020-04-18 15:30:24 -0400

    #1 created unit test for new class

---
+++ Tests/BurnSoft.UniversalTests/BSConversion_Test.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by burnsoft on 4/18/20.
+//
+
+import Foundation
+import XCTest
+import BurnSoft_Universal
+
+class BurnSoftMath_Test: XCTestCase {
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        //currentDateTime = Date()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_convertBOOLtoString() {
+        self.measure {
+            let value = BSConversion.convertBOOLtoString(value: true)
+            let didPass : Bool = (value == "true")
+            XCTAssert(didPass)
+        }
+    }
+    func test_convertBOOLYNtoString() {
+        self.measure {
+            let value = BSConversion.convertBOOLYNtoString(value: true)
+            let didPass : Bool = (value == "Yes")
+            XCTAssert(didPass)
+        }
+    }
+}
